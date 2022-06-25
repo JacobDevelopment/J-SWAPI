3f3d483c4a8501ba7cdda1ffcd261d572dff6655
VCSJacob <jkingster@protonmail.com>
2022-06-25 12:59:01 -0500

    Added more convenience methods.

--- src/main/java/io/jking/jswapi/action/RequestAction.java
+++ src/main/java/io/jking/jswapi/action/RequestAction.java
@@ -1,17 +1,34 @@
 package io.jking.jswapi.action;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jking.jswapi.resources.BaseResource;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public interface RequestAction<T extends BaseResource> {
 
-    T getById(int id) throws IOException;
+    T getById(final int id) throws IOException;
 
     List<T> getAll() throws IOException;
 
     List<T> getBySearch(final String searchQuery) throws IOException;
+
+    default List<T> getByFilter(final Predicate<T> filter) throws IOException {
+        return getAll()
+                .stream()
+                .filter(filter)
+                .collect(Collectors.toList());
+    }
+
+    default Optional<T> getFirstBySearch(final String searchQuery) throws IOException {
+        return getBySearch(searchQuery)
+                .stream()
+                .findFirst();
+    }
 
 }
---
+++ src/test/java/io/jking/jswapi/ConvenienceTest.java
@@ -0,0 +1,33 @@
+package io.jking.jswapi;
+
+import io.jking.jswapi.resources.impl.People;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+public class ConvenienceTest {
+
+    public static void main(String[] args) throws IOException {
+        API api = new API.Builder().build();
+        getByFilter(api);
+        getFirst(api);
+    }
+
+    private static void getByFilter(API api) throws IOException {
+        final List<People> people = api.people().getByFilter(
+                filter -> filter.getHairColor().equalsIgnoreCase("blond")
+        );
+
+        people.forEach(person -> System.out.println(person.getName()));
+    }
+
+    private static void getFirst(API api) throws IOException {
+        api.people().getFirstBySearch("anakin skywalker")
+                .ifPresent(person -> {
+                    System.out.println(person.getName());
+                });
+    }
+
+
+}
